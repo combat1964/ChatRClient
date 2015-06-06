63c41a2bba9d3434a557b77a296f4538d2843593
Emre Guneyler <eguneys@hotmail.com>
2015-06-06 15:52:22 +0300

    user model added, user join added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,8 @@ import Room from 'chatr/models/chat-room';
 
 import Message from 'chatr/models/chat-message';
 
+import User from 'chatr/models/chat-user';
+
 export default Ember.Controller.extend({
   
   initRoom: function(users, messages) {
@@ -20,9 +22,18 @@ export default Ember.Controller.extend({
     room.addMessage(sender, msg);
   },
 
+  userJoin: function(name) {
+    var room = this.get('room');
+
+    var user = User.buildNew(name);
+
+    room.userJoin(user);
+  },
+
   actions: {
     sendChat: function(msg) {
       this.addMessage('ME', msg);
+      this.userJoin(msg);
     }
   }
 });
